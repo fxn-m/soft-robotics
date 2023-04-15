409acd5bccafc9edd801ae299139934780cdd95e
Felix Newport-Mangell <felixnewportmangell98@gmail.com>
2023-04-16 00:27:10 +0100

    updates

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def update_plot(frame_count, angles_list):
  
 if __name__ == '__main__':
 
-    file_path = f'./recordings/IMG_{3677+13}.mov'
+    file_path = f'./recordings/IMG_{3685}.mov'
     vc = cv.VideoCapture(file_path)
 
     if (vc.isOpened() == False):
